ac8d043481aff442419f8428eb6e0f145eaeeb6c
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-10-22 14:13:50 +0100

    Bump stdump to version v1.2.1

--- os/download.sh
+++ os/download.sh
@@ -4,7 +4,7 @@ set -e
 # Downloads a release of ccc and puts all the stdump executables in the right
 # places. This file is called by the CI workflow for putting out new releases.
 
-version='v1.2'
+version='v1.2.1'
 
 pushd $(dirname -- "$0")
 rm -f "ccc_$(echo $version)_linux.zip"
